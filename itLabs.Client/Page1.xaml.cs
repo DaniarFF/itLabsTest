70f342e95e9187de40ed1068768f545f9bea65bf
Dan <monreal007@gmail.com>
2025-01-21 16:34:20 +0400

    Add second page

---
+++ itLabs.Client/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace itLabs
+{
+  /// <summary>
+  ///  Логика взаимодействия для первой страницы приложения
+  /// </summary>
+  public partial class Page1 : Page
+  {
+    /// <summary>
+    ///  Конструктор
+    /// </summary>
+    public Page1()
+    {
+      InitializeComponent();
+    }
+
+    /// <summary>
+    ///  Переход на вторую страницу
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void NavigateToPage2(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Page2());
+    }
+  }
+}
