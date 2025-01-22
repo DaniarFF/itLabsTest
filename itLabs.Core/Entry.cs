9775f7f04b506927ec1d2cce833412bc80a4a866
Dan <monreal007@gmail.com>
2025-01-22 18:30:35 +0400

    refactoring core structure

---
+++ itLabs.Core/Entry.cs
@@ -0,0 +1,14 @@
+﻿using itLabs.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace itLabs.Core;
+
+public static class Entry
+{
+  public static IServiceCollection AddCoreIntegration(this IServiceCollection serviceCollection)
+  {
+    serviceCollection.AddScoped<ICustomerService, CustomerService>();
+
+    return serviceCollection;
+  }
+}
---
+++ itLabs.Core/Services/CustomerService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using itLabs.Core.Models;
+using itLabs.Repository;
+
+namespace itLabs.Core.Services;
+
+public class CustomerService : ICustomerService
+{
+  private readonly ICustomerRepository repository;
+
+  public CustomerService(ICustomerRepository repository)
+  {
+    this.repository = repository;
+  }
+
+  public void Add(Customer customer)
+  {
+    var entity = new CustomerEntity
+    {
+      Id = customer.Id,
+      Name = customer.Name,
+      Email = customer.Email,
+      Phone = customer.Phone
+    };
+
+    repository.Add(entity);
+  }
+
+  public IEnumerable<Customer> GetAll()
+  {
+    var entities = repository.GetAll();
+
+    var customers = entities.Select(entity => new Customer
+    {
+      Phone = entity.Phone,
+      Email = entity.Email,
+      Name = entity.Name,
+      Id = entity.Id
+    });
+
+    return customers;
+  }
+
+  public void Delete(int id)
+  {
+    repository.Delete(id);
+  }
+}
