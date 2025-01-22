2dc3ac0b8c55a31ec253712fa2623d192f78b3df
Dan <monreal007@gmail.com>
2025-01-22 18:32:05 +0400

    refactoring repository structure
    update logic

---
+++ itLabs.Repository/CustomerRepositoryMock.cs
@@ -0,0 +1,38 @@
+﻿using itLabs.Core.Models;
+
+namespace itLabs.Repository
+{
+  public class CustomerRepositoryMock : ICustomerRepository
+  {
+    private List<CustomerEntity> customers = new List<CustomerEntity>()
+    {
+      new CustomerEntity
+      {
+        Id = 1, 
+        Name = "Хаилов Сергей Викторович", 
+        Email = "jsdsd@jsds.com", Phone = "+7934567890"
+      },
+      new CustomerEntity 
+      { 
+        Id = 2, Name = "Плотников Андрей Владимирович", 
+        Email = "adnfe@example.com", 
+        Phone = "+7944567894" },
+    };
+    
+    public IEnumerable<CustomerEntity> GetAll()
+    {
+      return customers;
+    }
+    
+    public void Add(CustomerEntity entity)
+    {
+      customers.Add(entity);
+    }
+    
+    public void Delete(int id)
+    {
+      customers.RemoveAll(x => x.Id == id);
+    }
+  }
+}
+
---
+++ itLabs.Repository/Entry.cs
@@ -0,0 +1,14 @@
+﻿using itLabs.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace itLabs.Repository;
+
+public static class Entry
+{
+  public static IServiceCollection AddRepositoryIntegration(this IServiceCollection serviceCollection)
+  {
+    serviceCollection.AddScoped<ICustomerRepository, CustomerRepositoryMock>();
+
+    return serviceCollection;
+  }
+}
